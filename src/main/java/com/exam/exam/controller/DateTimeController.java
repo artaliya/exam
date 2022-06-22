2ca3dbf179c0b04533e9c76ca7552221abdc5f02
artaliya <8belkind8@gmail.com>
2022-06-22 20:41:12 +0200

    Implemented controller and service for date and time

---
+++ src/main/java/com/exam/exam/controller/DateTimeController.java
@@ -0,0 +1,30 @@
+package com.exam.exam.controller;
+
+import com.exam.exam.service.DateTimeService;
+import com.fasterxml.jackson.databind.ser.std.DateTimeSerializerBase;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DateTimeController {
+
+    private final DateTimeService dateTimeService;
+
+    public DateTimeController(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+
+    @GetMapping("/today")
+    public String getToday() {
+        return dateTimeService.getToday();
+    }
+
+    @GetMapping("/now")
+    public String getNowTime(@RequestParam String format) {
+        System.out.println(format);
+        return dateTimeService.getNowTime(format);
+    }
+}
